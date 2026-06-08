c3159b9f5290f12ecf2247f2a688ef16b6f914fb
mdxzubair <102023370+mdxzubair@users.noreply.github.com>
2026-06-09 02:58:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
  // ═══════════ AMBIENT PARTICLE SYSTEM ═══════════
     (function(){
-        const canvas = document.getElementById('particle-canvas');
+
+    if(window.innerWidth < 768){
+        return;
+    }
+
+    const canvas =
+    document.getElementById('particle-canvas');
         const ctx = canvas.getContext('2d');
         let W, H, particles = [];
 
